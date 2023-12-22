c98498ab82dc375e5af346ea0a82270264be34ff
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-12-22 11:54:50 +0530

    Backend: bit formatting

--- backend/src/models/video.models.js
+++ backend/src/models/video.models.js
@@ -1,41 +1,44 @@
-import {Schema, model} from "mongoose"
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+import { Schema, model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({
+const videoSchema = new Schema(
+  {
     videoFile: {
-        true: String, // Cloudinary URL
-        required: [true, "Video url is required"]
+      true: String, // Cloudinary URL
+      required: [true, "Video url is required"],
     },
     thumbnail: {
-        true: String, // Cloudinary URL
-        required: [true, "Thumbnail url is required"]
+      true: String, // Cloudinary URL
+      required: [true, "Thumbnail url is required"],
     },
     title: {
-        true: String,
-        required: [true, "Video Title is required"]
+      true: String,
+      required: [true, "Video Title is required"],
     },
     description: {
-        true: String,
-        required: [true, "Description is required"]
+      true: String,
+      required: [true, "Description is required"],
     },
     duration: {
-        type: Number, // Cloudinary time
-        required: [true, "Duration of the video is required"]
+      type: Number, // Cloudinary time
+      required: [true, "Duration of the video is required"],
     },
     views: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     isPublished: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
     owner: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, {timestamps: true})
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-videoSchema.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate);
 
-export const Video = model("Video", videoSchema)
+export const Video = model("Video", videoSchema);
